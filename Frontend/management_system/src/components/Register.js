432efcda6c921bb03c26cc6d1078b678ed868a52
Jay <djay1128@yahoo.com>
2023-10-08 19:18:09 -0400

    Navbar-Display name, Add profile, move register; authSlice-add user; Add User Dunmmy Data

--- Frontend/management_system/src/components/Register.js
+++ Frontend/management_system/src/components/Register.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Role } from "../constants/Role";
+import { USERS } from "../constants/User";
+var USERID = 10;
 function Register() {
   const [isSameFlag, setFlag] = useState(true);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    const [username, password, confirmPassword, role] = event.target;
-    if(password.value !== confirmPassword.value){
+    if(password !== confirmPassword){
       setFlag(false);
       return;
     }else{
       setFlag(true);
     }
+
     const newUser = {
-      username: username.value,
-      password: password.value,
-      role: role.value
+      id: USERID++,
+      name: name,
+      username: username,
+      password: password,
+      role: role,
+      phone: undefined,
+      company: undefined
     }
-    // console.log(JSON.stringify(newUser));
+    console.log(JSON.stringify(newUser));
     // ToDO: add fetch() to post newForm to /addUser API 
+    USERS.push(newUser);
+    setName("");
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+    setRole("");
   };
 
   return (
@@ -26,19 +42,19 @@ function Register() {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="registerName">Name</label>
-          <input type="text" className="form-control" id="registerName" required/>
+          <input type="text" className="form-control" id="registerName" onChange={e => setName(e.target.value)} value={name} required/>
         </div>
         <div className="form-group">
           <label htmlFor="registerUsername">Username</label>
-          <input type="text" className="form-control" id="registerUsername" required/>
+          <input type="text" className="form-control" id="registerUsername"  onChange={e => setUsername(e.target.value)} value={username} required/>
         </div>
         <div className="form-group">
           <label htmlFor="registerPassword1">Password</label>
-          <input type="password" className="form-control" id="registerPassword1" required/>
+          <input type="password" className="form-control" id="registerPassword1" onChange={e => setPassword(e.target.value)} value={password} required/>
         </div>
         <div className="form-group">
           <label htmlFor="registerPassword2">Confirm Password</label>
-          <input type="password" className="form-control" id="registerPassword2" required/>
+          <input type="password" className="form-control" id="registerPassword2" onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} required/>
           {
             isSameFlag ? null :
             <div className="alert alert-danger" role="alert">
@@ -46,23 +62,22 @@ function Register() {
             </div>
           }
         </div>
-        <div className="form-group">
+        <div className="form-group" onChange={e => setRole(e.target.value)}>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerResidentRadioOption" value="Resident" required/>
+            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerResidentRadioOption" value={Role.Resident} checked={role === Role.Resident} readOnly required/>
             <label className="form-check-label" htmlFor="registerResidentRadioOption">Resident</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerHOARadioOption" value="HOA"/>
+            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerHOARadioOption" value={Role.HOA} checked={role === Role.HOA} readOnly/>
             <label className="form-check-label" htmlFor="registerHOARadioOption">HOA</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerThirdPartyCompanyRadioOption" value="ThirdPartyCompany"/>
+            <input className="form-check-input" type="radio" name="registerRadioOption" id="registerThirdPartyCompanyRadioOption" value={Role.ThirdPartyCompany} checked={role === Role.ThirdPartyCompany} readOnly/>
             <label className="form-check-label" htmlFor="registerThirdPartyCompanyRadioOption">Third Party Company</label>
           </div>
         </div>
         <div className="container row justify-content-between">
           <button type="submit" className="btn btn-primary">Submit</button>
-          <Link className="nav-link" to="/login">Go Back</Link>
         </div>
     </form>
   </div>
--- Frontend/management_system/src/slice/authSlice.js
+++ Frontend/management_system/src/slice/authSlice.js
@@ -1,13 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { USERS } from "../constants/User";
 
 export const authSlice = createSlice({
   name: 'isLoggedIn',
   initialState: {
     value: false,
+    user: null
   },
   reducers: {
-    login: (state) => {
-      state.value = true;
+    login: (state, action) => {
+      const {username, password} = action.payload;
+      const user = USERS.find(user => user.username === username && user.password === password);
+      if(user){
+        state.value = true;
+        state.user = user;
+      }
     },
     logout: (state) => {
       state.value = false;
